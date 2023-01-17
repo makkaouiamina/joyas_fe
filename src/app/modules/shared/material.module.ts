3fd6b2d8abc7c487bb3668f74cd21fc722fddcf6
ELHasnae <hassnalaoui21@gmail.com>
2023-01-17 18:40:15 +0100

    upload file shared/material.module.ts

---
+++ src/app/modules/shared/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDialogModule
+
+  ],
+})
+export class MaterialModule { }
